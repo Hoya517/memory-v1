0062eb20ebb0eec9d67ffe6a547c0c2ef641dc29
hoya <sv05159@naver.com>
2023-03-18 22:55:15 +0900

    [자동 구성(Auto Configuration)] 순수 라이브러리 만들기

--- src/test/java/memory/MemoryFinderTest.java
+++ src/test/java/memory/MemoryFinderTest.java
@@ -1,2 +1,16 @@
-package memory;public class MemoryFinderTest {
+package memory;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class MemoryFinderTest {
+
+    @Test
+    void get() {
+        MemoryFinder memoryFinder = new MemoryFinder();
+        Memory memory = memoryFinder.get();
+        System.out.println("memory = " + memory);
+        assertThat(memory).isNotNull();
+    }
 }
